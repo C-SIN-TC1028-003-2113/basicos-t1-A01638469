c02649c158bd8c085423029bdd13384d1a529a5c
A01638469 <A01638469@itesm.mx>
2021-08-14 15:43:23 +0000

    tarea 1

--- assignments/03Promedio/src/exercise.py
+++ assignments/03Promedio/src/exercise.py
@@ -1,6 +1,11 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    cal_suma = float(input('Calificación de la materia: '))
+    cal_suma += float(input('Calificación de la materia: '))
+    cal_suma += float(input('Calificación de la materia: '))
+    cal_suma += float(input('Calificación de la materia: '))
+    promedio_final = cal_suma / 4
+    print('El promedio es: ' + str(promedio_final))
 
 
 if __name__ == '__main__':
